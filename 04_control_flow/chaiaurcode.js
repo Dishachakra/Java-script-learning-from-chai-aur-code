e5b57906fa8b2aff70f94fcc93ba5d2929ec0815
Disha Chakraborty <disha.chakrabortydev@gmail.com>
2025-11-11 18:17:10 +0530

    control_flow

---
+++ 04_control_flow/chaiaurcode.js
@@ -0,0 +1,14 @@
+function one(){
+    console.log("One");
+    two()
+}
+function two(){
+    console.log("two");
+    three()
+}
+function three(){
+    console.-("three");
+}
+one()
+two()
+three()
---
+++ 04_control_flow/one.js
@@ -0,0 +1,16 @@
+const isUserloggedIn = true
+const temperature = 50
+
+// if( isUserloggedIn ){
+//     console.log("Executed");
+    
+// }
+if( temperature < 50){
+    console.log("Less than 50");
+}
+if( temperature == 50 ){
+        console.log("Equal to 50");
+}
+else{
+    console.log("temperature is greater than 50");
+}
